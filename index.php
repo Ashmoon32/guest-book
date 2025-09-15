89c856e8da1572fe863b9e220dc8d0a34a26c925
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-09-15 10:23:46 +0000

    Finsih index.php

--- index.php
+++ index.php
@@ -9,7 +9,37 @@
 <body>
     <h2>Guest Book</h2>
 
-    <form action="index.php" method="POST" id="guestform"></form>
+    <form action="index.php" method="POST" id="guestForm">
+
+    <input type="text" name="name" placeholder="Your Name" required>
+    <textarea name="message" placeholder="Your Message" required></textarea>
+    <button type="submit">Submit</button>
+
+    </form>
     
+    <h3>Message:</h3>
+    <div id="messages">
+        <?php
+            if ( file_exists("messages.txt")) {
+                $lines = file("messages.txt");
+                foreach ($lines as $line){
+                    echo "<p>" .htmlspecialchars($line) . "</p>";
+                }
+            }
+            ?>
+    </div>
+
+    <script src="script.js"></script>
 </body>
 </html>
+
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = htmlspecialchars($_POST["name"]);
+        $messsage = htmlspecialchars($_POST["message"]);
+        $entry = "$name: $message\n";
+        file_put_contents("messages.txt", $entry, FILE_APPEND);
+        header("Location: index.php");
+        exit();
+    }
+?>
