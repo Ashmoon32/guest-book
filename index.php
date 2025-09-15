a3d6e8a01293596d1785ab37fe53af749ebaaa99
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-09-15 10:10:07 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guest Book</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h2>Guest Book</h2>
+
+    <form action="index.php" method="POST" id="guestform"></form>
+    
+</body>
+</html>
